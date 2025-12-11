43eafa323caf3a02b9a827aa6df6239aef9950c9
trybadisch <76852449+trybadisch@users.noreply.github.com>
2025-12-11 14:23:46 +0100

    day 3 - p2

--- day3/day3.py
+++ day3/day3.py
@@ -1,16 +1,36 @@
 with open("input.txt", 'r') as f:
     lines = [x.strip() for x in f.readlines()]
 
-count = []
-
 def part1():
+    count = []
     for i in lines:
-        max = 0
+        max_ = 0
         for idj, j in enumerate(i):
             for k in i[idj+1:]:
-                max = int(j+k) if max < int(j+k) else max
-        count.append(max)
+                max_ = int(j+k) if max_ < int(j+k) else max_
+        count.append(max_)
+
+    print(sum(count))
+
+def part2():
+    count = []
+    for i in lines:
+        max_ = ""
+        ind = 0
+
+        for r in range(1,12+1):
+
+            try:
+                m_num = max([int(x) for x in i[ind:-(12-r)]])
+            except:
+                m_num = max([int(x) for x in i[ind:]])
+
+            max_ += str(m_num)
+            ind += i[ind:].index(str(m_num))+1
+
+        count.append(int(max_))
 
     print(sum(count))
 
 part1()
+part2()
