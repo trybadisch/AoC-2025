1d88a353159491de32e4d732f8a04c6df348d9b6
trybadisch <76852449+trybadisch@users.noreply.github.com>
2025-12-11 15:27:34 +0100

    refactoring

--- day2/day2.py
+++ day2/day2.py
@@ -1,51 +1,43 @@
 with open("input.txt", 'r') as f:
     lines = [x.strip().split(',') for x in f.readlines()]
 
-def part1():
-    count = []
-    for i in lines[0]:
+count1 = []
+count2 = []
 
-        s = int(i.split('-')[0])
-        e = int(i.split('-')[1])
+for i in lines[0]:
 
-        for j in range(s,e+1):
-            if len(str(j)) % 2 == 0:
-                half = int(len(str(j))/2)
-                chars = str(j)
+    t_count = []
 
-                if chars[0:half] == chars[half:]:
-                    count.append(j)
+    s = int(i.split('-')[0])
+    e = int(i.split('-')[1])
 
-    print(sum(count))
+    for j in range(s,e+1):
 
-def part2():
-    count = []
-    for i in lines[0]:
-
-        t_count = []
-
-        s = int(i.split('-')[0])
-        e = int(i.split('-')[1])
-
-        for j in range(s,e+1):
-            length = len(str(j))
+        # Part 1
+        if len(str(j)) % 2 == 0:
+            half = int(len(str(j))/2)
             chars = str(j)
 
-            for str1 in range(0,length):
-                for str2 in range(str1+1, length):
-                    len_dup = len(chars[str1:str1+str2])
+            if chars[0:half] == chars[half:]:
+                count1.append(j)
+        
+        # Part 2
+        length = len(str(j))
+        chars = str(j)
 
-                    if length % len_dup == 0:
-                        div = int(length / len_dup)
-                        rest = str(chars[str1:str1+str2]) * div
+        for str1 in range(0,length):
+            for str2 in range(str1+1, length):
+                len_dup = len(chars[str1:str1+str2])
 
-                        if rest == chars:
-                            t_count.append(j)
+                if length % len_dup == 0:
+                    div = int(length / len_dup)
+                    rest = str(chars[str1:str1+str2]) * div
 
-        for c in set(t_count):
-            count.append(c)
+                    if rest == chars:
+                        t_count.append(j)
 
-    print(sum(count))
+    for c in set(t_count):
+        count2.append(c)
 
-part1()
-part2()
+print(sum(count1))
+print(sum(count2))
